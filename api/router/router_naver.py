e740f6c51b606ddba127cd4e3edf0bf5565fe295
wkdalswns0427 <pawpaw0427@gmail.com>
2023-08-04 18:30:16 +0900

    frame

--- api/router/router_naver.py
+++ api/router/router_naver.py
@@ -7,6 +7,10 @@ from utils.crawler import CrawlerNaverMap
 router = APIRouter()
 crawler = CrawlerNaverMap()
 
+
+# get time
 @router.get('/get_cafe_list', status_code=status.HTTP_200_OK)
 def search_by_keyword(search_key : Optional[str]):
     res = crawler.main(search_keyword=search_key)
+
+    for key, values in res:
